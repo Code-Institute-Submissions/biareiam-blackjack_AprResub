e2b242be99d5b3f832057984ee767849459ddbaf
biareiam <biareiam@gmail.com>
2021-09-26 08:40:02 +0000

    craeting a give player a card function

--- run.py
+++ run.py
@@ -1,7 +1,35 @@
-# Create a deck of cards and a player dealer hand
-# Create a function which deals the cards
-# Take each players hand (players and dealer) and calculate the total on each hand
-# check for the winner of the game
-# Game loop -  to start the game
+# to make sure each time the cards a dealt is it random 
 
-import random # to make sure each time the cards a dealt is it random 
+import random
+
+cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+playercards = []
+
+def getcard():
+    """
+    This function will get a random card each time
+    """
+    card = random.randrange(13) # 0 inded basis
+    return cards[card]
+
+def giveplayercard():
+    card = getcard()
+    playercards.append(card)
+
+
+
+
+def startgame():
+    # it will print 2 random cards, and append them to the playercards hand each time. 
+    for i in range(2):
+        giveplayercard()
+    print(playercards)   
+    asnwer = input("Do you want another card?") 
+
+    if asnwer.lower() == 'yes':
+        giveplayercard()
+    else:
+        pass    
+
+
+    
