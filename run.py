3530ad217025df12b8cf8b6d2404df733cd3dccb
biareiam <biareiam@gmail.com>
2021-09-26 10:13:59 +0000

    checking if the values are 21 or close to it and who wins

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # to make sure each time the cards a dealt is it random 
 
 import random
+import time # give it some time
 
 cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 specialcards = ['J','Q','K']
@@ -31,9 +32,6 @@ def givecard(cards):
 #     dealercards.append(card)
 
 
-
-
-
 # it will increment 1 if it is an Aces and if it an special card it will increment 10
 # if it is none of it, it will increment the value of the card itself. 
 def gethandvalue(cards):
@@ -47,17 +45,35 @@ def gethandvalue(cards):
             counter += int(card)    
     return counter
 
+def printdealerhand(cards, firsttime = False):
+    print(f'Dealer\'s deck: ',end='')
+    for card in cards:
+        print(f'[{card}]',end='')
+        if firsttime:
+           print(f'[?]',end='')
+           break
+    print()
+
 
 def dealerturn():
     while True:
-        if gethandvalue (dealercards) > 17:
+        if gethandvalue(dealercards) >= 17:
            return False
         else:
            givecard(dealercards)
-           print(dealercards)
+           printdealerhand(dealercards)
+           time.sleep(1)
            if gethandvalue(dealercards) > 21:
                return True
-               
+
+
+
+def printplayerhand(cards):
+    print(f'Your deck: ', end='')
+    for card in cards:
+        print(f'[{card}]',end='')
+    print()    
+
 
 # while playing the user can decide to hit or stay
 def playerturn():
@@ -65,38 +81,53 @@ def playerturn():
         answer = input("Will you hit or will you stay?")
         if answer.lower() == 'hit':
            givecard(playercards)
-           print(playercards)
+           printplayerhand(playercards)
+           time.sleep(1)
            value = gethandvalue(playercards)
            if value > 21:
-               return True   
+               return True
+           elif value == 21:
+                return True 
         else:
            return False
 
 
-
-    
-
 # once the player goes over 21, the game is over.
 def gameover():
-    print ('Oh! you lost it')  
+    if gethandvalue(playercards) == 21:
+        print('You win! Congrats')
+    else:
+        print ('Oh! you lose it')    
        
 
 def startgame():
-    # it will print 2 random cards, and append them to the playercards hand each time. 
     for i in range(2):
         givecard(playercards)
-    print(playercards)
-
-    for i in range(2):
         givecard(dealercards)
 
+    printplayerhand(playercards)
+    time.sleep(1)
+    printdealerhand(dealercards, True)
+    time.sleep(1)
+  
     if playerturn():
         gameover()
+        return
 
     if dealerturn():
-        print('uhu! You win!')   
+        print('uhu! You win!')
 
-    print(playercards)
+    playervalue =  gethandvalue(playercards)
+    dealervalue = gethandvalue(dealercards)   
+
+    if playervalue < dealervalue:
+        print('oh!You lose')
+    elif playervalue == dealervalue:
+        print('You draw!')
+    else:
+        print('You win the game! Congrats!')    
+
+    printplayerhand(playercards)
     print(gethandvalue(playercards))    
 
 
