e7ca8fec98c94d2d7345fe0ced50aa61261f3870
biareiam <biareiam@gmail.com>
2021-09-26 07:21:09 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Create a deck of cards and a player dealer hand
+# Create a function which deals the cards
+# Take each players hand (players and dealer) and calculate the total on each hand
+# check for the winner of the game
+# Game loop -  to start the game
+
+import random # to make sure each time the cards a dealt is it random 
