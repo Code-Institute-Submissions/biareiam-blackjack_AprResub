5ca40cf7e1c05ee472d7b5798e5c3404394ce4bd
biareiam <biareiam@gmail.com>
2021-09-26 09:10:40 +0000

    printing the total and a message if the user is over 21, so the game is over

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import random
 
 cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+specialcards = ['J','Q','K']
 playercards = []
 
 def getcard():
@@ -16,14 +17,34 @@ def giveplayercard():
     card = getcard()
     playercards.append(card)
 
+
+def gethandvalue(cards):
+    counter = 0
+    for card in cards:
+        if card == 'A':
+            counter +=1
+        elif card in specialcards:
+            counter += 10
+        else:
+            counter += int(card)    
+    return counter
+
+
 def playerturn():
     while True:
         answer = input("Will you hit or will you stay?")
         if answer.lower() == 'hit':
            giveplayercard()
            print(playercards)
+           value = gethandvalue(playercards)
+           if value > 21:
+               return True   
         else:
-           return 
+           return False
+
+def gameover():
+    print ('Oh! you lost it')  
+       
 
 def startgame():
     # it will print 2 random cards, and append them to the playercards hand each time. 
@@ -31,8 +52,11 @@ def startgame():
         giveplayercard()
     print(playercards)
 
-    playerturn ()
-    print(playercards)    
+    if playerturn():
+        gameover()
+
+    print(playercards)
+    print(gethandvalue(playercards))    
 
 
 startgame()
