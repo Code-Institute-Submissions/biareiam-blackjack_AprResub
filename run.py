ec22eb4f2c3fbb975b082e5bfc0b24dad5d196bf
biareiam <biareiam@gmail.com>
2021-09-26 09:32:03 +0000

    creating a dealer hand

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import random
 cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 specialcards = ['J','Q','K']
 playercards = []
+dealercards = []
 
 def getcard():
     """
@@ -13,11 +14,28 @@ def getcard():
     card = random.randrange(13) # 0 inded basis
     return cards[card]
 
-def giveplayercard():
+def givecard(cards):
     card = getcard()
-    playercards.append(card)
+    cards.append(card)
 
 
+# the player will get 2 cards
+#def giveplayercard():
+#    card = getcard()
+#    playercards.append(card)
+
+
+# the dealer will get 2 cards
+# def givedealercard():
+#     card = getcard()
+#     dealercards.append(card)
+
+
+
+
+
+# it will increment 1 if it is an Aces and if it an special card it will increment 10
+# if it is none of it, it will increment the value of the card itself. 
 def gethandvalue(cards):
     counter = 0
     for card in cards:
@@ -30,11 +48,23 @@ def gethandvalue(cards):
     return counter
 
 
+def dealerturn():
+    while True:
+        if gethandvalue (dealercards) > 17:
+           return False
+        else:
+           givecard(dealercards)
+           print(dealercards)
+           if gethandvalue(dealercards) > 21:
+               return True
+               
+
+# while playing the user can decide to hit or stay
 def playerturn():
     while True:
         answer = input("Will you hit or will you stay?")
         if answer.lower() == 'hit':
-           giveplayercard()
+           givecard(playercards)
            print(playercards)
            value = gethandvalue(playercards)
            if value > 21:
@@ -42,6 +72,11 @@ def playerturn():
         else:
            return False
 
+
+
+    
+
+# once the player goes over 21, the game is over.
 def gameover():
     print ('Oh! you lost it')  
        
@@ -49,11 +84,17 @@ def gameover():
 def startgame():
     # it will print 2 random cards, and append them to the playercards hand each time. 
     for i in range(2):
-        giveplayercard()
+        givecard(playercards)
     print(playercards)
+
+    for i in range(2):
+        givecard(dealercards)
 
     if playerturn():
         gameover()
+
+    if dealerturn():
+        print('uhu! You win!')   
 
     print(playercards)
     print(gethandvalue(playercards))    
