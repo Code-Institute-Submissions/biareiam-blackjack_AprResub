71be97bd5c1bb3aa7e1f3cbc2976063afb51d3b6
biareiam <biareiam@gmail.com>
2021-10-13 18:41:32 +0000

    validating the code

--- README.md
+++ README.md
@@ -1,31 +1,86 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# BlackJack/21 command-line application game
 
-Welcome biareiam,
+The aim of this project was to build a command-line game of 21 or BlackJack.  The goal of this game is to amount card values as near to 21 as possible without exceeding or 'busting' it. It is simple If the dealer gets closer to 21 points or 21 exactly, it wins the game. If the player is the one who gets closer to 21 or 21 exactly, they are the winner. This game will not be dealing with bets. 
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+Here is the live version of my project
 
-## Reminders
+ ![command-line game](/assets/images/tux.png)
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
+## How to play
 
-## Creating the Heroku app
+The game is played with a deck of 52 cards. In this version, the player will be only playing against the dealer and no bets will be placed. 
+After shuffling the deck, the player and the dealer will get two cards each. The player will only be able to see his/her cards. Cards should be dealt one at a time. Throughout the game the player can choose to hit, which means get an extra card or stay, at this stage either the player or dealer receive another card and the results are printed. At this point, the player can then see the dealer’s cards, how much it was added to and who won the game. As previously mentioned, the winner is the one who got as close as possible to 21 without busting.
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+Card Values are as follows:
+- Ace: It has a value of either 11 or 1 as needed to reach 21 without busting, in this project, Ace values 1. 
+- 2-10 -  the card  value is the same as the number printed in the cards, example. 2 is 2, 3 is 3 and so on. 
+- Jacks, Queens, and Kings each have a value of 10.
 
-1. `heroku/python`
-2. `heroku/nodejs`
+The rules of the original game can be found here : [Wikipedia](https://en.wikipedia.org/wiki/Blackjack) 
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+## Features
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+* Welcoming
+    * The player will be asked to enter his/her name and a welcome message will be printed along with a brief explanation of the rules of the game.
 
-Connect your GitHub repository and deploy as normal.
+     ![Tux, the Linux mascot](/assets/images/tux.png) 
 
-## Constraints
+* Random card generation
+    * As mentioned previously, the dealer and the player will get two random cards in the beginning of the game.
+    * The player will have the ability to see one of the dealer’s cards at the start of the game. 
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+     ![Tux, the Linux mascot](/assets/images/tux.png)
 
------
-Happy coding!
+* Accepts user input
+    * Once the cards are displayed, the player can choose to hit or stay
+    * The player is able to hit as many times as they want as long it does not go over 21. Once it happens, the game is over and the results are printed. 
+
+     ![Tux, the Linux mascot](/assets/images/tux.png)
+
+* Results
+    * Once the game is over, the results board is printed. The player will then be able to see the dealer’s cards and how much the cards add up to.
+
+     ![Tux, the Linux mascot](/assets/images/tux.png)
+
+## Future Features
+
+ * Make the cards being printed as “actual” cards.
+ * More than one player. 
+
+## Testing
+
+I have manually tested this project by doing the following:
+
+- Starting and playing the game several times. 
+- Given invalid inputs
+- Tested in my local terminal and the Code Institute Heroku terminal.
+
+## Deployment 
+
+This project was deployed using Code Institute’s mock terminal for Heroku.
+    - Steps for deployment were: 
+    - Fork or clone this repository
+    - Create a new Heroku app
+    - Set the builtbacks to Python and NodeJS in that order
+    - Link Heroku app to the repository
+    - Clicking on Deploy
+
+
+## Credits
+
+* Code Institute for the deployment terminal
+*  [Bicyclecards](https://bicyclecards.com/how-to-play/blackjack/) for the rules of the blackjack/21 game.
+*   [Brilliant](https://brilliant.org/wiki/programming-blackjack/) - Helped to have a better understading of the logic of the game.
+
+
+## Acknowledgements
+
+I appreciate the support, feedback and guidance of the following people throughout this project: my mentor Victor Miclovich, the tutors and the community on slack.
+This is for educational use.
+
+
+
+
+
+
+
--- run.py
+++ run.py
@@ -1,175 +1,152 @@
-# to make sure each time the cards a dealt is it random 
-
+#Importing Random Library 
 import random
-import time # give it some time
 
+#Defining Variables 
+Suits = ('♥', '♦', '♠', '♣')
+Ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+Values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10,
+         'Q':10, 'K':10, 'A':1}
+playing = True
 
-cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-specialcards = ['J','Q','K']
-playercards = []
-dealercards = []
+#Creating Card Class
+class Card():
+    def __init__(self,suit,rank):
+        # Suit 
+        self.suit = suit 
+        # Rank
+        self.rank = rank 
+        # Corresponding Value  by indexing
+        self.value = Values[rank]
+    
+    #returns rank of suit if printed 
+    def __str__(self):
+        return self.rank +" "+ self.suit
 
+class Deck():
+    #Creates an empty list to which all cards are appended to
+    def __init__(self):
+        self.all_cards = []
+        #The double for loop creates 13 ranks for each Suit
+        for suit in Suits:
+            for rank in Ranks:
+                #Create the Card Object
+                created_card = Card(suit,rank)
+                #Appends the created cards one by one to the list.
+                self.all_cards.append(created_card)
+                
+    def __str__(self):
+        deck_comp = ''
+        for card in self.all_cards:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has:  " + deck_comp
+            
+    def shuffle(self):
+        #Shuffles the list containing the cards
+        random.shuffle(self.all_cards)
+   
+    def deal(self):
+        #pops the item of a list based on the index specified
+        single_card = self.all_cards.pop()
+        return single_card 
 
-def getcard():
-    """
-    This function will get a random card each time
-    """
-    card = random.randrange(13) # 0 inded basis
-    return cards[card]
-
-def givecard(cards):
-    card = getcard()
-    cards.append(card)
-
-
-# the player will get 2 cards
-#def giveplayercard():
-#    card = getcard()
-#    playercards.append(card)
-
-
-# the dealer will get 2 cards
-# def givedealercard():
-#     card = getcard()
-#     dealercards.append(card)
-
-
-# it will increment 1 if it is an Aces and if it an special card it will increment 10
-# if it is none of it, it will increment the value of the card itself. 
-def gethandvalue(cards):
-    counter = 0
-    for card in cards:
-        if card == 'A':
-            counter +=1
-        elif card in specialcards:
-            counter += 10
-        else:
-            counter += int(card)    
-    return counter
-
-def printdealerhand(cards, firsttime = False):
-    print(f'Dealer\'s deck: ',end='')
-    for card in cards:
-        print(f'[{card}]',end='')
+#Creating Hand Class that refers to the cards in Player and Dealer's Hand
+class Hand():
+    def __init__(self):
+        self.cards = []  # starting with an empty list
+        self.value = 0   # starting  with zero value
+        self.aces = 0    # adding an attribute to keep track of aces
+    
+    def add_card(self,card):
+        #After appending cards to Hand, Value of Hand is also modified.
+        self.cards.append(card)
+        self.value = self.value + card.value
         
-        if firsttime:
-           print(f'[?]',end='')
-           break
-    print()
+        if card.rank == 'Ace':
+            self.aces += 1
+            
+    def adjust_for_ace(self):
+        #If the value of hand exceeds 21 because ace value is taken as 11, subtract 10 from the total value. 
+        while self.value > 21 and self.aces:
+            self.value -= 10 
+            self.aces -= 1 
 
 
-def dealerturn():
+ 
+
+
+#Function that draws cards to hand of PLayer or Dealer while adusting for ace value
+def hit(deck,hand):
+    single_card = deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
+
+#Asks user if he is going to stand or hit 
+def hit_or_stand(deck,hand):
+    global playing  # to control an upcoming while loop, global variable can be changed.
+    
     while True:
-        if gethandvalue(dealercards) >= 17:
-           return False
-        else:
-           givecard(dealercards)
-           printdealerhand(dealercards)
-           print()
-           time.sleep(1)
-           if gethandvalue(dealercards) > 21:
-               return True
-
-
-
-def printplayerhand(cards):
-    print(f'Your deck: ', end='')
-    for card in cards:
-        print(f'[{card}]',end='')
-    print()    
-
-
-# while playing the user can decide to hit or stay
-def playerturn():
-    while True:
-        answer = input("Will you hit or will you stay?")
-        if answer.lower() == 'hit':
-           givecard(playercards)
-           printplayerhand(playercards)
-           print()
-           time.sleep(1)
-           value = gethandvalue(playercards)
-           print()
-           if value > 21:
-               return True
-           elif value == 21:
-                return True 
-        else:
-           return False
-
-
-def printstats():
-    print('-----STATS-----')
-    printplayerhand(playercards)
-    print(f'Total value: {gethandvalue(playercards)}')
-    printdealerhand(dealercards)
-    print(f'Total value: {gethandvalue(dealercards)}')
-    print()
-    print('Thanks for playing 😍😍😍😍')
-    print('---------------')
-
-
-
-
-
-
-# once the player goes over 21, the game is over.
-def gameover():
-    if gethandvalue(playercards) == 21:
-        print('You win! Congrats 😊 👏')
+        print('\n')
+        x = input("Do you want to hit or stay? Enter 'h' or 's' :")
         
-    else:
-        print ('Oh! you lost 😒')
+        if x[0].lower() =='h':
+            hit(deck,hand)
+            print("\nPlayer Hits !")
+            
+        elif x[0].lower() == 's':
+            print("\nPlayer Stands, Dealer's Turn")
+            playing = False
+            
+        else:
+            print("\nSorry, I did not understand that, please enter h or s")
+            continue
+
+        break
+
+#Function that shows some of the cards in hand 
+def show_some(player,dealer):
+    print("\nCurrent cards in hand:")
+    print("\nDealer's Hand: ")
+    print("  <Hidden Card!>")
+    print(f"  {dealer.cards[1]}")
+    
+    print("\nPlayer's Hand: ")
+    for card in player.cards:
+        print(f"  {card}")
+
+#Function that shows all cards in hand 
+def show_all(player,dealer):
+
+    print("\nFINAL RESULT :")
+    print("\nDealer's Hand: ")
+    for card in dealer.cards:
+        print(f"  {card}")
+       
+    print(f"  Value of Dealer's hand is {dealer.value}")
+    
+    print("\nPlayer's Hand: ")
+    for card in player.cards:
+        print(f"  {card}")
         
-    printstats() 
+    print(f"  Value of Player's hand is {player.value}")
 
+#Function for when Player is busted
+def player_busts(player,dealer):
+    print("\nPlayer busted! Sorry,the dealer won!! 😒")
+ 
 
-def startgame():
-   
-    print()
-    print('The rules are:')
-    print('The objective is to get a hand total of closer to 21 than the dealer without going over 21.')
-    print('At the start of the game, the player and the dealer receive two cards each.')
-    print('Kings, Queens, and Jacks are worth 10 points./nAces are worth 1 or 11 points.\nCards 2 through 10 are worth their face value.\nHit to take another card.\nStay to stop taking cards.')
-    print()
-    print('Good luck!😏')
-   
-   
+#Function for when Player WINS 
+def player_wins(player,dealer):
+    print("\nPlayer won the game!! 😁")
+    
 
-    time.sleep(3)
-    for i in range(2):
-        givecard(playercards)
-        givecard(dealercards)
-
-    printplayerhand(playercards)
-    time.sleep(1)
-    printdealerhand(dealercards, True)
-    time.sleep(1)
+#Function for when Player WINS cause Dealer is Busted 
+def dealer_busts(player,dealer):
+    print("\nThe dealer busted! Congrats the player won the game !!! 👏😁")
   
 
-    print()
-    if playerturn():
-        gameover()
-        return
-
-    if dealerturn():
-        print('👏')
-
-    playervalue = gethandvalue(playercards)
-    dealervalue = gethandvalue(dealercards)   
-
-    if playervalue < dealervalue:
-        print('You lose 😒')
-     
-    elif playervalue == dealervalue:
-        print('You draw!😅')
-       
-    else:
-        print('You win the game! Congrats!😁 👏') 
-        
-    printstats()  
-   
-
+#Function for when Dealer WINS 
+def dealer_wins(player,dealer):
+    print("\nOh no!The dealer won! 😒")
 
 
 def create_player():
@@ -182,22 +159,101 @@ def create_player():
         name = input("Please enter a username: ")
         print()
         print("⭐ ⭐ Welcome to the BlackJack game", name + " !⭐ ⭐")
+        print()
         if name != '':
             break
         else:
             print("Enter a valid name.\n")
 
 
+#When it is a tie.
+def push():
+    print("\nDealer and Player tie! 😅")
 
-create_player()
-startgame()
+while True:
+    
+    # Printing an opening statement
+    create_player()
+    print('The rules are:')
+    print('The objective is to get a hand total of closer to 21 than the dealer without going over 21.')
+    print('At the start of the game, the player and the dealer receive two cards each.')
+    print('Kings, Queens, and Jacks are worth 10 points./nAces are worth 1 or 11 points.\nCards 2 through 10 are worth their face value.\nHit to take another card.\nStay to stop taking cards.')
+    print()
+    print('Good luck!😏')
+    
+    
+    # Creating & shuffling the deck, dealing two cards to each player
+    New_Deck = Deck()
+    New_Deck.shuffle()
+    
+    Dealer_hand = Hand()
+    Player_hand = Hand()
+        
+    Player_hand.add_card(New_Deck.deal())
+    Dealer_hand.add_card(New_Deck.deal())
+    Player_hand.add_card(New_Deck.deal())
+    Dealer_hand.add_card(New_Deck.deal())
+    
+   
 
+    # Shows cards (but keeps one dealer card hidden)
+    show_some(Player_hand,Dealer_hand)
+    
+    while playing:  #variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(New_Deck,Player_hand)
+        
+        # Shows cards (but keeps one dealer card hidden)
+        show_some(Player_hand,Dealer_hand)
+        
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        if Player_hand.value > 21:
+            show_all(Player_hand,Dealer_hand)
+            player_busts(Player_hand,Dealer_hand) 
+            break
 
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    if Player_hand.value <= 21:
+        
+        while Dealer_hand.value < 17:
+            hit(New_Deck,Dealer_hand)
+            continue
+            
+        
+        # Test different winning scenarios
+            # Run different winning scenarios
+        if Dealer_hand.value > 21:
+            dealer_busts(Player_hand,Dealer_hand)
+        
+        elif Dealer_hand.value > Player_hand.value:
+            dealer_wins(Player_hand,Dealer_hand)
 
+        elif Dealer_hand.value < Player_hand.value:
+            player_wins(Player_hand,Dealer_hand)
 
-
-
-
-
+        else:
+            push()
+    
+        # Show all cards
+        show_all(Player_hand,Dealer_hand)
+        
 
     
+    # Ask to play again
+   
+    play_again = input("\nWould you like to play again ? Enter y for yes and n for no: ")
+    
+    if play_again[0].lower() == 'y':
+        playing = True 
+        continue 
+    elif play_again[0].lower() == 'n':
+        playing = False 
+        #End Message
+        print("\nThank You for Playing Blackjack !!")
+        break
+    else:
+        print("You have not entered a valid Input, Game Ends !")
+        print("Until next time!")
+        break
+#End of Game Code 
